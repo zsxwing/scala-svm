db4af865de9e2a9b5bae23070c1c2dea3cd944f9
zsxwing <zsxwing@gmail.com>
2013-05-23 20:11:39 +0800

    Add the kernel function

---
+++ src/main/scala/me/iamzsx/scala/svm/Kernel.scala
@@ -0,0 +1,145 @@
+package me.iamzsx.scala.svm
+
+import java.io._
+
+import scala.math._
+import scala.io._
+
+object KernelType extends Enumeration {
+  type KernelType = Value
+  val LINEAR = Value("linear")
+  val POLY = Value("poly")
+  val RBF = Value("rbf")
+  val SIGMOID = Value("sigmoid")
+  val PRECOMPUTED = Value
+
+  def dot(x: List[SVMNode], y: List[SVMNode]): Double = {
+    if (x == Nil || y == Nil) 0
+    else {
+      if (x.head.index == y.head.index) {
+        x.head.value * y.head.value + dot(x.tail, y.tail)
+      } else {
+        if (x.head.index < y.head.index) dot(x.tail, y)
+        else dot(x, y.tail)
+      }
+    }
+  }
+
+  def powi(base: Double, times: Int) = {
+    assert(times >= 0)
+    var tmp = base
+    var ret = 1.0
+    var t = times
+    while (t > 0) {
+      if ((t & 1) == 1) ret *= tmp;
+      tmp *= tmp;
+      t >>= 1;
+    }
+    ret
+  }
+
+}
+
+import KernelType._
+/**
+ * @see http://en.wikipedia.org/wiki/Support_vector_machine
+ */
+trait Kernel {
+  def kernelType: KernelType
+  def apply(x: List[SVMNode], y: List[SVMNode]): Double
+}
+
+/**
+ * @see http://en.wikipedia.org/wiki/Support_vector_machine
+ */
+case class LinearKernel extends Kernel {
+
+  override def kernelType = LINEAR
+
+  override def apply(x: List[SVMNode], y: List[SVMNode]) = {
+    dot(x, y)
+  }
+
+  override def toString = "kernel_type " + kernelType.toString
+}
+
+/**
+ *
+ */
+class PolynomialKernel(val gamma: Double, val coef0: Double, val degree: Int) extends Kernel {
+  require(degree >= 0) // Why degree == 0 is valid?
+
+  override def kernelType = POLY
+  override def apply(x: List[SVMNode], y: List[SVMNode]) = {
+    powi(gamma * dot(x, y) + coef0, degree);
+  }
+  override def toString = Array(
+    "kernel_type " + kernelType.toString,
+    "degree " + degree,
+    "gamma " + gamma,
+    "coef0 " + coef0).mkString("\n")
+}
+
+/**
+ * @see http://en.wikipedia.org/wiki/Support_vector_machine#Nonlinear_classification
+ * @see http://en.wikipedia.org/RBF_kernel
+ */
+class RBFKernel(val gamma: Double) extends Kernel {
+  require(gamma >= 0) // Why gamma == 0 is valid?
+
+  override def kernelType = RBF
+
+  override def apply(x: List[SVMNode], y: List[SVMNode]) = {
+    def rbf(x: List[SVMNode], y: List[SVMNode], sum: Double): Double = {
+      if (x == Nil && y == Nil) exp(-gamma * sum);
+      else if (x == Nil)
+        rbf(Nil, y.tail, sum + y.head.value * y.head.value);
+      else if (y == Nil)
+        rbf(x.tail, Nil, sum + x.head.value * x.head.value);
+      else {
+        if (x.head.index == y.head.index) {
+          rbf(x.tail, y.tail, (x.head.value - y.head.value) * (x.head.value - y.head.value) + sum)
+        } else {
+          if (x.head.index < y.head.index) rbf(x.tail, y, sum + x.head.value * x.head.value)
+          else rbf(x, y.tail, sum + y.head.value * y.head.value)
+        }
+      }
+    }
+    rbf(x, y, 0)
+  }
+
+  override def toString = Array(
+    "kernel_type " + kernelType.toString,
+    "gamma " + gamma).mkString("\n")
+}
+
+/**
+ * @see http://en.wikipedia.org/wiki/Support_vector_machine#Nonlinear_classification
+ */
+class SigmoidKernel(val gamma: Double, val coef0: Double) extends Kernel {
+  require(gamma >= 0) // Why gamma == 0 is valid?
+
+  override def kernelType = SIGMOID
+
+  override def apply(x: List[SVMNode], y: List[SVMNode]) = {
+    tanh(gamma * dot(x, y) + coef0);
+  }
+
+  override def toString = Array(
+    "kernel_type " + kernelType.toString,
+    "gamma " + gamma,
+    "coef0 " + coef0).mkString("\n")
+}
+
+class PrecomputedKernel extends Kernel {
+
+  override def kernelType = PRECOMPUTED
+
+  override def apply(x: List[SVMNode], y: List[SVMNode]) = {
+    // TODO
+    0
+  }
+
+  override def toString = "kernel_type " + kernelType.toString
+
+}
---
+++ src/test/scala/me/iamzsx/scala/svm/KernelSuite.scala
@@ -0,0 +1,111 @@
+package me.iamzsx.scala.svm
+
+import scala.math._
+
+import org.scalatest._
+import org.scalatest.matchers._
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.junit.Assert._
+
+import KernelType._
+
+@RunWith(classOf[JUnitRunner])
+class KernelTypeSuite extends FunSuite {
+
+  val DELTA = 10E-6
+
+  test("dot") {
+    val x = List(new SVMNode(1, 0.4), new SVMNode(2, 0.5))
+    val y = List(new SVMNode(1, 0.9), new SVMNode(3, 0.4))
+    assertEquals(0.36, dot(x, y), DELTA)
+  }
+
+  test("dot with normal") {
+    val x = List(new SVMNode(1, 0.4), new SVMNode(2, 0.5))
+    val y = List(new SVMNode(1, 0.9), new SVMNode(2, 0.4))
+    assertEquals(0.56, dot(x, y), DELTA)
+  }
+
+  test("dot with empty") {
+    val x = List(new SVMNode(1, 0.4), new SVMNode(2, 0.5))
+    val y = List(new SVMNode(4, 0.9), new SVMNode(3, 0.4))
+    assertEquals(0, dot(x, y), DELTA)
+  }
+
+  test("powi") {
+    assertEquals(16, powi(2.0, 4), DELTA)
+    assertEquals(1, powi(2.0, 0), DELTA)
+    assertEquals(1.21, powi(1.1, 2), DELTA)
+    assertEquals(1.23 * 1.23 * 1.23 * 1.23 * 1.23, powi(1.23, 5), DELTA)
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class LinearKernelSuite extends FunSuite {
+
+  val DELTA = 10E-6
+
+  test("toString") {
+    assertEquals("kernel_type linear", new LinearKernel().toString)
+  }
+
+}
+
+@RunWith(classOf[JUnitRunner])
+class PolynomialKernelSuite extends FunSuite {
+
+  val DELTA = 10E-6
+
+  test("toString") {
+    assertEquals("kernel_type poly\n" +
+      "degree 3\n" +
+      "gamma 1.0\n" +
+      "coef0 2.0", new PolynomialKernel(1.0, 2.0, 3).toString)
+  }
+
+  test("apply") {
+    val x = List(new SVMNode(1, 0.4), new SVMNode(2, 0.5))
+    val y = List(new SVMNode(1, 0.9), new SVMNode(3, 0.4))
+
+    assertEquals(pow(1.0 * 0.36 + 2.0, 3), new PolynomialKernel(1.0, 2.0, 3)(x, y), DELTA)
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class RBFKernelSuite extends FunSuite {
+
+  val DELTA = 10E-6
+
+  test("toString") {
+    assertEquals("kernel_type rbf\n" +
+      "gamma 1.0", new RBFKernel(1.0).toString)
+  }
+
+  test("apply") {
+    val x = List(new SVMNode(1, 0.4), new SVMNode(2, 0.5))
+    val y = List(new SVMNode(1, 0.9), new SVMNode(3, 0.4))
+
+    assertEquals(exp(-2.0 * 0.66), new RBFKernel(2.0)(x, y), DELTA)
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class SigmoidKernelSuite extends FunSuite {
+
+  val DELTA = 10E-6
+
+  test("toString") {
+    assertEquals("kernel_type sigmoid\n" +
+      "gamma 1.0\n" +
+      "coef0 2.0", new SigmoidKernel(1.0, 2.0).toString)
+  }
+
+  test("apply") {
+    val x = List(new SVMNode(1, 0.4), new SVMNode(2, 0.5))
+    val y = List(new SVMNode(1, 0.9), new SVMNode(3, 0.4))
+
+    assertEquals(tanh(1.0 * 0.36 + 2.0), new SigmoidKernel(1.0, 2.0)(x, y), DELTA)
+  }
+}
