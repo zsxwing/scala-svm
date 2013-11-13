ab16a206b3f41f9b1da6a6f221d1a9bde18025e4
zsxwing <zsxwing@gmail.com>
2013-11-13 17:36:28 +0800

    Add a unit test for NuSVCModel

--- src/test/scala/me/iamzsx/scala/svm/SVMModelSuite.scala
+++ src/test/scala/me/iamzsx/scala/svm/SVMModelSuite.scala
@@ -16,6 +16,7 @@ class SVMModelSuite extends FunSuite with BeforeAndAfter {
   val DELTA = 10E-6
 
   test("OneClassModel") {
+    // ./svm-train -s 2 -t 0 -r 0.2 test.data test.model
     val param = new SVMParameter(LinearKernel, 0, 0)
     val supportVector = Array(
       SupportVector.fromString("1:1 2:22.08 3:11.46"),
@@ -28,6 +29,7 @@ class SVMModelSuite extends FunSuite with BeforeAndAfter {
   }
 
   test("NuSVRModel") {
+    // ./svm-train -s 4 -t 0 -r 0.2 test.data test.model
     val param = new SVMParameter(LinearKernel, 0, 0)
     val supportVector = Array(
       SupportVector.fromString("1:1 2:22.08 3:11.46"),
@@ -40,6 +42,7 @@ class SVMModelSuite extends FunSuite with BeforeAndAfter {
   }
 
   test("EpsilonSVRModel") {
+    // ./svm-train -s 3 -t 0 -r 0.2 test.data test.model
     val param = new SVMParameter(LinearKernel, 0, 0)
     val supportVector = Array(
       SupportVector.fromString("1:19"),
@@ -52,6 +55,7 @@ class SVMModelSuite extends FunSuite with BeforeAndAfter {
   }
 
   test("CSVCModel") {
+    // ./svm-train -s 0 -t 0 -r 0.2 test.data test.model
     val param = new SVMParameter(LinearKernel, 0, 0)
     val supportVector = Array(
       Array(
@@ -85,6 +89,47 @@ class SVMModelSuite extends FunSuite with BeforeAndAfter {
     assertPredictInstance("1 1:1.000000 2:22.080000 3:11.460000", model)
     assertPredictInstance("2 1:19", model)
     assertPredictInstance("3 2:34 3:15", model)
+    assertPredictInstance("4 1:10 3:22", model)
+  }
+
+  test("NuSVCModel") {
+    // ./svm-train -s 1 -t 0 -r 0.2 test.data test.model
+    val param = new SVMParameter(LinearKernel, 0, 0)
+    val supportVector = Array(
+      Array(
+        SupportVector.fromString("2:13.08 3:7")),
+      Array(
+        SupportVector.fromString("1:19")),
+      Array(
+        SupportVector.fromString("2:34 3:15"), SupportVector.fromString("2:-1")),
+      Array(
+        SupportVector.fromString("1:10 3:22")))
+    val coefficients = new Coefficients(
+      Array(
+        Array(
+          null,
+          (Array(0.003441828960374912), Array(-0.002121210192839219)),
+          (Array(1.934584364555769), Array(-0.7975824366423717, -1.137001927913397)),
+          (Array(0.004031555793506938), Array(-0.004031555793506938))),
+        Array(
+          null,
+          null,
+          (Array(0.005537785840454479), Array(-0.0001336706927006254, -0.005404115147753854)),
+          (Array(0.003539823008849557), Array(-0.003539823008849557))),
+        Array(
+          null,
+          null,
+          null,
+          (Array(0.001020943323660012, 0.003034858921016748), Array(-0.00405580224467676)))))
+    val rho = Array(-0.2425, 1.67644, -0.733569, 0.999141, -0.39469, -1.03168)
+    val label = 1 to 4
+    val model = new NuSVCModel(4, param, supportVector, coefficients, rho, label)
+
+    assertPredictInstance("1 1:1.000000 2:22.080000 3:11.460000", model)
+    assertPredictInstance("1 2:13.080000 3:7.0000", model)
+    assertPredictInstance("2 1:19", model)
+    assertPredictInstance("3 2:34 3:15", model)
+    assertPredictInstance("3 2:-1", model)
     assertPredictInstance("4 1:10 3:22", model)
   }
 
